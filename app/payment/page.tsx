cff7cdafeeee1accf38db10c3d794fd3084d3303
Naral <77269201+NaralC@users.noreply.github.com>
2023-07-04 22:49:20 +0100

    Skeleton table

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,9 +1,9 @@
-import { TableDemo } from "@/components/custom/table-demo";
+import { DataTable } from "./data-table";
 
 export default function Payment() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-24">
-      
+      <DataTable columns={[]} data={[]} />
     </main>
   );
 }
